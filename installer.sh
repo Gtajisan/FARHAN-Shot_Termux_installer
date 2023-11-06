6c7fc0730adf3b8050dbd1d689a4dcd0d0054b60
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-11-06 19:29:30 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,8 +2,9 @@
 pkg install -y root-repo 
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-git clone --depth 1 https://github.com/gtajisan/FARHAN-Shot2 FARHAN-Shot2
-chmod +x FARHAN-Shot2/FARHAN-Shot2.py
-sudo python FARHAN-Shot/FARHAN-Shot.py -i wlan0 -K
 
-printf "###############################################\n#  All done! Now you can run FARHAN-Shot2 with\n#   sudo python FARHAN-Shot/FARHAN-Shot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd FARHAN-Shot2 && git pull)\n###############################################\n"
+git clone --depth 1 https://github.com/gtajisan/FARHAN-Shot FARHAN-Shot
+
+chmod +x FARHAN-Shot/WifiHack
+
+printf "###############################################\n#  All done! Now you can run FARHAN-Shot with\n#   sudo python FARHAN-Shot/FARHAN-Shot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd FARHAN-Shot && git pull)\n###############################################\n"
