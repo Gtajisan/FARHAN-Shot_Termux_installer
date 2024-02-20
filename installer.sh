1ff3cf96a8ce278a7b1cce8710c8ca10cfd0819a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-02-20 13:46:51 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,6 +5,6 @@ git clone --depth 1 https://github.com/gtajisan/FARHAN-Shot2 FARHAN-Shot2
 
 pkg install -y git tsu python wpa-supplicant pixiewps iw openssl
 
-chmod +x FARHAN-Shot/WifiHack
+chmod +x FARHAN-Shot/FARHAN-Shot
 
 printf "###############################################\n#  All done! Now you can run FARHAN-Shot with\n#   sudo python FARHAN-Shot/FARHAN-Shot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd FARHAN-Shot && git pull)\n###############################################\n"
